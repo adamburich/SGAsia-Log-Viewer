2b396b1eb77aafc2e8218e62188554f8703776aa
adamburich <adamburich@gmail.com>
2023-06-07 01:48:48 -0400

    Added XHR request that gets the mongoDB

--- flaskr/static/script/MongoDBQuery.js
+++ flaskr/static/script/MongoDBQuery.js
@@ -1,17 +1,35 @@
-var MongoClient = require('mongodb').MongoClient;
+//var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
 
+function sendMongoDBQuery_ON_HOLD(){
+//    let text = document.getElementById("mongoQuery");
+//    let query = text.value;
+//
+//    MongoClient.connect(url, function(err, db) {
+//        if (err) throw err;
+//        var dbo = db.db("mydb");
+//        dbo.collection("logs").find(query).toArray(function(err, result) {
+//            if (err) throw err;
+//            console.log(result);
+//            db.close();
+//        });
+//    });
+}
+
 function sendMongoDBQuery(){
-    let text = document.getElementById("mongoQuery");
-    let query = text.value;
-    
-    MongoClient.connect(url, function(err, db) {
-        if (err) throw err;
-        var dbo = db.db("mydb");
-        dbo.collection("logs").find(query).toArray(function(err, result) {
-            if (err) throw err;
-            console.log(result);
-            db.close();
-        });
-    });
+var settings = {
+  "url": "https://6p5txxhkagnbr3dxk54iz4n3cu0pvjub.lambda-url.ap-southeast-1.on.aws/",
+  "method": "POST",
+  "timeout": 0,
+  "headers": {
+    "Content-Type": "application/json"
+  },
+  "data": JSON.stringify({
+    "statement.context.extensions.http://gamestrax&46;com/extensions/session": "6bf76a62"
+  }),
+};
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
 }
